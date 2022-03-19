79bf0f9dabf6705081c7c9c5faf043bab2c731de
Alexey Lamkin <98019904+alexkrill@users.noreply.github.com>
2022-03-20 01:15:41 +0300

    Delete string-analyze.js

--- string-analyze.js
+++
@@ -1,9 +0,0 @@
-const path = "/users/download/index.html";
-
-function stringValidation (path) {
-  if (path.slice(-5) == '.html') {
-      return true;
-  }
-}
-
-console.log(stringValidation(path));
